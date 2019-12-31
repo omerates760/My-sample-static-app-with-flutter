e711da6f055296c7bd3dcfc2b60b37a2280d7628
omerates760 <omerates760@gmail.com>
2019-12-31 13:48:36 +0300

    landscape screen changed.

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -31,53 +31,64 @@ class _AuthPageState extends State<AuthPage> {
                       Colors.black.withOpacity(0.3), BlendMode.dstATop))),
           padding: EdgeInsets.all(10.0),
           child: Center(
-              child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                TextField(
-                  decoration: InputDecoration(labelText: 'E-Mail',filled: true,fillColor: Colors.white),
-                  keyboardType: TextInputType.emailAddress,
-                  onChanged: (String value) {
-                    setState(() {
-                      _emailValue = value;
-                    });
-                  },
-                ),
-                SizedBox(height: 10.0,),
-                TextField(
-                  decoration: InputDecoration(labelText: 'Password',filled: true,fillColor: Colors.white),
-                  obscureText: true,
-                  onChanged: (String value) {
-                    setState(() {
-                      _passwordValue = value;
-                    });
-                  },
-                ),
-                SwitchListTile(
-                  value: _acceptTerms,
-                  onChanged: (bool value) {
-                    setState(() {
-                      _acceptTerms = value;
-                    });
-                  },
-                  title: Text('Accept Terms'),
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                RaisedButton(
-                  color: Theme.of(context).primaryColor,
-                  textColor: Colors.white,
-                  child: Text('LOGIN'),
-                  onPressed: () {
-                    print(_emailValue);
-                    print(_passwordValue);
-                    Navigator.pushReplacementNamed(context, '/goHomePage');
-                  },
-                ),
-              ],
-            ),
-          )),
+            child: SingleChildScrollView(
+                child: Container(
+                  width: MediaQuery.of(context).size.width*0.7,
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(
+                        labelText: 'E-Mail',
+                        filled: true,
+                        fillColor: Colors.white),
+                    keyboardType: TextInputType.emailAddress,
+                    onChanged: (String value) {
+                      setState(() {
+                        _emailValue = value;
+                      });
+                    },
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                        labelText: 'Password',
+                        filled: true,
+                        fillColor: Colors.white),
+                    obscureText: true,
+                    onChanged: (String value) {
+                      setState(() {
+                        _passwordValue = value;
+                      });
+                    },
+                  ),
+                  SwitchListTile(
+                    value: _acceptTerms,
+                    onChanged: (bool value) {
+                      setState(() {
+                        _acceptTerms = value;
+                      });
+                    },
+                    title: Text('Accept Terms'),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  RaisedButton(
+                    color: Theme.of(context).primaryColor,
+                    textColor: Colors.white,
+                    child: Text('LOGIN'),
+                    onPressed: () {
+                      print(_emailValue);
+                      print(_passwordValue);
+                      Navigator.pushReplacementNamed(context, '/goHomePage');
+                    },
+                  ),
+                ],
+              ),
+            )),
+          ),
         ));
   }
 }
